4fdfc65754aaa70fc605cb1847dd34c1ef5c6c5f
sverlon <75929507+sverlon@users.noreply.github.com>
2020-12-13 04:38:12 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,51 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { Platform, StyleSheet, Text, View, Button } from 'react-native';
+import Login from './app/Login';
+import WelcomeScreen from './app/WelcomeScreen';
+import SignUp from './app/SignUp'
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  
+  return (
+    <NavigationContainer>
+      <Stack.Navigator 
+      initialRouteName="WelcomeScreen"
+      screenOptions={{ headerShown: false }}
+      >
+        <Stack.Screen name="WelcomeScreen" component={WelcomeScreen} />
+        <Stack.Screen name="LoginScreen" component={Login} />
+        <Stack.Screen name="SignupScreen" component={SignUp} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  logoContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    top: '10%',
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  logoFont: {
+    fontFamily: 'AppleSDGothicNeo-SemiBold',
+    fontSize: 70,
+  },
+  logoSubtitleFont: {
+    fontFamily: 'AppleSDGothicNeo-Light',
+    fontSize: 20,
+  }
+});
