de31f1e9b228c0cb548c189176c2dd6d88f8c1a6
sverlon <75929507+sverlon@users.noreply.github.com>
2021-01-27 03:19:31 -0500

    Now connects to websocket-based back-end

--- App.js
+++ App.js
@@ -3,20 +3,32 @@ import React from 'react';
 import { Platform, StyleSheet, Text, View, Button } from 'react-native';
 import Login from './app/Login';
 import WelcomeScreen from './app/WelcomeScreen';
-import SignUp from './app/SignUp'
+import SignUp from './app/SignUp';
+import Tabs from "./app/Tabs"
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 const Stack = createStackNavigator();
 
+var socket = new WebSocket("ws://192.168.1.68:8080"); 
+
+socket.onopen = function(event) {
+  console.log("Connection Established");
+  socket.send("Frontend client successfully connected to Websocket Server");
+}
+
+socket.onmessage = function(event) {
+  console.log(event.data);
+}
+
 export default function App() {
-  
   return (
     <NavigationContainer>
       <Stack.Navigator 
       initialRouteName="WelcomeScreen"
       screenOptions={{ headerShown: false }}
       >
+        <Stack.Screen name="Home" component={Tabs} />
         <Stack.Screen name="WelcomeScreen" component={WelcomeScreen} />
         <Stack.Screen name="LoginScreen" component={Login} />
         <Stack.Screen name="SignupScreen" component={SignUp} />
